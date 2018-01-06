3aecdaa871f3c0cb77bd16cc9d4a069d0b9f403f
Ankit Solanki <ankit03june@gmail.com>
2018-01-06 19:42:27 +0530

    facebookscraper added - with access_token GET fucntion

--- facebookscraper.py
+++ facebookscraper.py
@@ -1,9 +1,21 @@
 import facebook
 import requests
+import json
+
+def get_fb_token(app_id, app_secret):           
+    
+    payload = {'grant_type': 'client_credentials', 'client_id': app_id, 'client_secret': app_secret}
+    file = requests.post('https://graph.facebook.com/oauth/access_token?', params = payload)
+    
+    data = file.json()
+    print(data)
+    access_token = data['access_token']
+
+    return (access_token)
 
 
 
-def facebook(access_token, user_id):
+def facebookscraper(access_token, user_id):
 
 	graph = facebook.GraphAPI(access_token)
 	profile = graph.get_object(user_id)
@@ -15,3 +27,9 @@ def facebook(access_token, user_id):
 	        posts = requests.get(posts['paging']['next']).json()
 	    except KeyError:
 	        break
+
+
+
+if __name__=="__main__":
+	s=get_fb_token(app_id, app_secret)
+	facebookscraper(s,"me")
