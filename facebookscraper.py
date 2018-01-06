3fc13e30a4b2d7d72244efa18651a2041f66be55
Ankit Solanki <ankit03june@gmail.com>
2018-01-06 23:54:58 +0530

    facebook scraper: user and app access token functions

--- facebookscraper.py
+++ facebookscraper.py
@@ -3,33 +3,30 @@ import requests
 import json
 
 def get_fb_token(app_id, app_secret):           
+    """returns app access token, using @param : app_id and app_secret"""
     
     payload = {'grant_type': 'client_credentials', 'client_id': app_id, 'client_secret': app_secret}
     file = requests.post('https://graph.facebook.com/oauth/access_token?', params = payload)
     
     data = file.json()
-    print(data)
-    access_token = data['access_token']
+    app_access_token = data['access_token']
 
-    return (access_token)
-
+    return (app_access_token)
 
 
-def facebookscraper(access_token, user_id):
 
+def facebookscraper(user_access_token, user_id="me"):
+	"""it takes user access token and retrive the posts"""
+	
 	graph = facebook.GraphAPI(access_token)
+	
 	profile = graph.get_object(user_id)
 	posts = graph.get_connections(profile['id'], 'posts')
 
 	while True:
+	    
 	    try:
 	        [print(post['message']) for post in posts['data']]
 	        posts = requests.get(posts['paging']['next']).json()
 	    except KeyError:
 	        break
-
-
-
-if __name__=="__main__":
-	s=get_fb_token(app_id, app_secret)
-	facebookscraper(s,"me")
