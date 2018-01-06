e53ada09813d9c8c6b5de3e82f9fe79cd456c9e3
Sumit Ghosh <sumit.ghosh32@gmail.com>
2018-01-06 18:48:33 +0530

    Add code to turn twitter data into IBM format

--- ibm.py
+++ ibm.py
@@ -1,6 +1,28 @@
 import json
 from watson_developer_cloud import PersonalityInsightsV3
+import twitterscraper
+from datetime import datetime
+from dateutil import parser
 
+
+
+def fetch_tweets(username):
+    tweets = twitterscraper.tweets(username)
+    content_items = {}
+    content_items['contentItems'] = []
+
+    for tweet in tweets:
+        item = {
+            'id': 'TW'+str(tweet.id),
+            'content': tweet.text,
+            'contenttype': 'text/plain',
+            'created': parser.parse(tweet.created_at).timestamp(),
+            'forward': tweet.retweeted, 
+            'reply': bool(tweet.in_reply_to_screen_name),
+        }
+        content_items['contentItems'].append(item)
+
+    print(content_items)
 
 
 def personality_insights(username, password, data):
@@ -12,3 +34,6 @@ def personality_insights(username, password, data):
     profile = personality_insights.profile(data, content_type='application/json',
                                            raw_scores=True, consumption_preferences=True)
     print(json.dumps(profile, indent=2))
+
+
+fetch_tweets('SkullTech101')
