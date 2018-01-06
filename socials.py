e8ff761015e3bc4a033cfc726cedf0bc3f3cf6e5
Sumit Ghosh <sumit.ghosh32@gmail.com>
2018-01-06 17:35:00 +0530

    Move the secrets to different file

--- socials.py
+++ socials.py
@@ -1,11 +1,15 @@
 import twitter
 import facebook
+import yaml
 
 
 
 def twitter(key, secret, username):
-    api = twitter.Api(consumer_key='urnMiSi6fhCKIGlSxGA9JVVp0',
-                      consumer_secret='IAY8FJknVRouNvFdlSFEMBAgvAlGygbpyp8SSAlFDYvRkszDZq',
+    with open('creds.yaml') as file:
+        creds = yaml.load(file)
+
+    api = twitter.Api(consumer_key=creds['Twitter']['ConsumerKey'],
+                      consumer_secret=creds['Twitter']['ConsumerSecret'],
                       access_token_key=key,
                       access_token_secret=secret)
     tweets = api.GetUserTimeline(screen_name=username)
