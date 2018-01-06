4299d68aa215b622984190d179dde3cba6086b32
Sumit Ghosh <sumit.ghosh32@gmail.com>
2018-01-06 16:50:06 +0530

    Trying out IBM Watson API

--- socials.py
+++ socials.py
@@ -3,14 +3,10 @@ import facebook
 
 
 
-def twitter(key, secret):
+def twitter(key, secret, username):
     api = twitter.Api(consumer_key='urnMiSi6fhCKIGlSxGA9JVVp0',
                       consumer_secret='IAY8FJknVRouNvFdlSFEMBAgvAlGygbpyp8SSAlFDYvRkszDZq',
                       access_token_key=key,
                       access_token_secret=secret)
-    tweets = api.GetHomeTimeline()
-    return [tweet.text for tweet in tweets]
-
-
-def facebook(token):
-    graph = facebook.GraphAPI(access_token=token)
+    tweets = api.GetUserTimeline(screen_name=username)
+    return tweets
