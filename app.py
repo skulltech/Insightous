07c130f4c5508d10054c69ae7db01624e9c3d254
Sumit Ghosh <sumit.ghosh32@gmail.com>
2018-01-10 18:16:56 +0530

    Fix the links in the templates

--- app.py
+++ app.py
@@ -7,17 +7,21 @@ app = Flask(__name__)
 app.secret_key = 'super secret key'
 
 
-@app.route('/insights')
+@app.route('/')
 def insights():
-	if not 'token' in session:
-		session['token'] = nucleus.get_token()
+    if not 'token' in session:
+        session['token'] = nucleus.get_token()
 
-	if request.method == 'POST':
-		person = nucleus.personality_insights(request.form['username'], request.session['token'])
-		return render_template('insights.html', person=person)
+    if request.method == 'POST':
+        person = nucleus.personality_insights(request.form['username'], request.session['token'])
+        return render_template('insights.html', person=person)
 
-    return render_template('insights.html', person=None)
+    return render_template('index.html', person=None)
 
+
+@app.route('/inside')
+def inside():
+    return render_template('inside.html')
 
 
 if __name__=='__main__':
